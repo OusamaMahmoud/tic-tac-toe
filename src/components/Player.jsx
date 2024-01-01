70434da74826342662abb439e6a314553c55b4fa
Osama Mahmoud <ousama.mah113@gmail.com>
2024-01-02 00:18:43 +0200

    editing of the ui

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,7 +4,7 @@ const Player = ({ winner, next }) => {
   return (
     <>
       {winner ? (
-        <div>Winner is {winner}</div>
+        <div className="bg-slate-800 text-lg font-serif text-white p-4  m-6">Winner is {winner}</div>
       ) : (
         <div className="bg-slate-800 text-lg font-serif text-white p-4  m-6">
           Next Player : {next ? "X" : "O"}
